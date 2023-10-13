bb56ae6ca16bef79d9eaf25a88933bbb3e483227
Jithu Byju <jithubyju1092002@gmail.com>
2023-10-13 22:49:13 +0530

    portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,226 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "HTML",
+    icon: html,
+    iconBg: "#E6DEDD",
+    date: "may 2023 - june 2023",
+    points: [
+      "Developing and maintaining web applications using Html and other related technologies.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Angular Developer",
+    icon: web,
+    iconBg: "#E6DEDD",
+    date: "june 2023 - july 2023",
+    points: [
+      "Developing and maintaining web applications using Angular and other related technologies.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "JavaScript",
+    icon: javascript,
+    iconBg: "#383E56",
+    date: "july 2023 - august 2023",
+    points: [
+      "Developing and maintaining web applications using javascript and other related technologies.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "React Developer",
+    icon: reactjs,
+    iconBg: "#383E56",
+    date: "august 2023 - september 2023",
+    points: [
+      "Developing and maintaining web applications using javascript and other related technologies.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    icon: figma,
+    iconBg: "#E6DEDD",
+    date: "september 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using mearn stack and other related technologies.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Interest Calculator",
+    description:
+      "Web-based platform that allows users to search interest by giving the amount,rate of interest and year with user validation",
+    tags: [
+    
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/jithu109",
+  },
+  {
+    name: "Netflix Clone",
+    description:
+      "It is a netflix clone application used to watch movies,webseries etc using react.And this web-application is a user frendly interface",
+    tags: [
+ 
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/jithu109",
+  },
+ 
+];
+
+export { services, technologies, experiences, testimonials, projects };
